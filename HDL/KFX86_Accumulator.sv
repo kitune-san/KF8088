548a58a583f2bdd67872eef3fe94a2b02dfa87e5
kitune-san <19359213+kitune-san@users.noreply.github.com>
2022-04-11 22:54:31 +0900

    Added ROT(SHT)_OPs if select_word=0.

--- HDL/KFX86_Accumulator.sv
+++ HDL/KFX86_Accumulator.sv
@@ -107,37 +107,58 @@ module KFX86_Accumulator (
                 out = source_1;
             end
             `ALU_OP_ROL: begin
-                {out_flags.c, out_tmp} = {source_1, source_1[15]};
-                out_flags.o = out_flags.c ^ out_tmp[15];
+                if (select_word)
+                    {out_flags.c, out_tmp     } = {source_1,      source_1[15]};
+                else
+                    {out_flags.c, out_tmp[7:0]} = {source_1[7:0], source_1[7]};
+                out_flags.o = out_flags.c ^ (select_word ? out_tmp[15] : out_tmp[7]);
                 out = out_tmp;
             end
             `ALU_OP_ROR: begin
-                {out_tmp, out_flags.c} = {source_1[0], source_1};
-                out_flags.o = ^out_tmp[15:14];
+                if (select_word)
+                    {out_tmp,      out_flags.c} = {source_1[0], source_1};
+                else
+                    {out_tmp[7:0], out_flags.c} = {source_1[0], source_1[7:0]};
+                out_flags.o = select_word ? ^out_tmp[15:14] : ^out_tmp[7:6];
                 out = out_tmp;
             end
             `ALU_OP_RCL: begin
-                {out_flags.c, out_tmp} = {source_1, out_flags.c};
-                out_flags.o = out_flags.c ^ out_tmp[15];
+                if (select_word)
+                    {out_flags.c, out_tmp     } = {source_1,      out_flags.c};
+                else
+                    {out_flags.c, out_tmp[7:0]} = {source_1[7:0], out_flags.c};
+                out_flags.o = out_flags.c ^ (select_word ? out_tmp[15] : out_tmp[7]);
                 out = out_tmp;
             end
             `ALU_OP_RCR: begin
-                {out_tmp, out_flags.c} = {out_flags.c, source_1};
-                out_flags.o = ^out_tmp[15:14];
+                if (select_word)
+                    {out_tmp,      out_flags.c} = {out_flags.c, source_1};
+                else
+                    {out_tmp[7:0], out_flags.c} = {out_flags.c, source_1[7:0]};
+                out_flags.o = select_word ? ^out_tmp[15:14] : ^out_tmp[7:6];
                 out = out_tmp;
             end
             `ALU_OP_SHL: begin
-                {out_flags.c, out_tmp} = {source_1, 1'b0};
-                out_flags.o = out_flags.c ^ out_tmp[15];
+                if (select_word)
+                    {out_flags.c, out_tmp     } = {source_1,      1'b0};
+                else
+                    {out_flags.c, out_tmp[7:0]} = {source_1[7:0], 1'b0};
+                out_flags.o = out_flags.c ^ (select_word ? out_tmp[15] : out_tmp[7]);
                 out = out_tmp;
             end
             `ALU_OP_SHR: begin
-                {out_tmp, out_flags.c} = {1'b0, source_1};
-                out_flags.o = source_1[15];
+                if (select_word)
+                    {out_tmp,      out_flags.c} = {1'b0, source_1};
+                else
+                    {out_tmp[7:0], out_flags.c} = {1'b0, source_1[7:0]};
+                out_flags.o = select_word ? source_1[15] : source_1[7];
                 out = out_tmp;
             end
             `ALU_OP_SAR: begin
-                {out_tmp, out_flags.c} = {source_1[15], source_1};
+                if (select_word)
+                    {out_tmp,      out_flags.c} = {source_1[15], source_1};
+                else
+                    {out_tmp[7:0], out_flags.c} = {source_1[7],  source_1[7:0]};
                 out_flags.o = 1'b0;
                 //out_flags.a = 1'bx;
                 out_flags.p = ~^out_tmp[7:0];
